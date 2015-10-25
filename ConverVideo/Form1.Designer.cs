67e573807f99888019f8895848d5e5f477394dfe
blueyi <blueyiniu@qq.com>
2015-10-25 15:45:15 +0800

    Deconvert

--- ConverVideo/Form1.Designer.cs
+++ ConverVideo/Form1.Designer.cs
@@ -29,8 +29,9 @@
         private void InitializeComponent()
         {
             this.openFileButton = new System.Windows.Forms.Button();
-            this.start = new System.Windows.Forms.Button();
+            this.convertToH = new System.Windows.Forms.Button();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.convertToS = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // openFileButton
@@ -43,26 +44,37 @@
             this.openFileButton.UseVisualStyleBackColor = true;
             this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
             // 
-            // start
+            // convertToH
             // 
-            this.start.Location = new System.Drawing.Point(168, 164);
-            this.start.Name = "start";
-            this.start.Size = new System.Drawing.Size(75, 23);
-            this.start.TabIndex = 1;
-            this.start.Text = "Start";
-            this.start.UseVisualStyleBackColor = true;
-            this.start.Click += new System.EventHandler(this.start_Click);
+            this.convertToH.Location = new System.Drawing.Point(58, 167);
+            this.convertToH.Name = "convertToH";
+            this.convertToH.Size = new System.Drawing.Size(102, 23);
+            this.convertToH.TabIndex = 1;
+            this.convertToH.Text = "S264 To H264";
+            this.convertToH.UseVisualStyleBackColor = true;
+            this.convertToH.Click += new System.EventHandler(this.start_Click);
             // 
             // openFileDialog
             // 
             this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // convertToS
+            // 
+            this.convertToS.Location = new System.Drawing.Point(236, 167);
+            this.convertToS.Name = "convertToS";
+            this.convertToS.Size = new System.Drawing.Size(102, 23);
+            this.convertToS.TabIndex = 1;
+            this.convertToS.Text = "H264 To S264";
+            this.convertToS.UseVisualStyleBackColor = true;
+            this.convertToS.Click += new System.EventHandler(this.convertToS_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(430, 256);
-            this.Controls.Add(this.start);
+            this.Controls.Add(this.convertToS);
+            this.Controls.Add(this.convertToH);
             this.Controls.Add(this.openFileButton);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -73,8 +85,9 @@
         #endregion
 
         private System.Windows.Forms.Button openFileButton;
-        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button convertToH;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button convertToS;
     }
 }
 
--- ConverVideo/Form1.cs
+++ ConverVideo/Form1.cs
@@ -92,5 +92,66 @@ namespace ConverVideo
             else
                 MessageBox.Show("No File");
         }
+
+        private void convertToS_Click(object sender, EventArgs e)
+        {
+            if (fileName.Length != 0)
+            {
+                FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.ReadWrite);
+                byte oldBuff = 0x53;
+                byte[] newBuff = new byte[4];
+                newBuff[0] = 0x48;
+                newBuff[1] = 0x32;
+                newBuff[2] = 0x36;
+                newBuff[3] = 0x34;
+                long[] offset = new long[2];
+                int count = 0;
+                for (long i = 0; i < fs.Length; ++i)
+                {
+                    byte currentBuf = Convert.ToByte(fs.ReadByte());
+                    if (newBuff[0] == currentBuf)
+                    {
+                        int j = 1;
+                        while (j < 4)
+                        {
+                            if (newBuff[j] != fs.ReadByte())
+                                break;
+                            j++;
+                        }
+                        if (j == 4)
+                        {
+                            fs.Seek(-4, SeekOrigin.Current);
+                            fs.WriteByte(oldBuff);
+                            count++;
+                        }
+                    }
+                    else if (oldBuff == currentBuf)
+                    {
+                        int j = 1;
+                        while (j < 4)
+                        {
+                            if (newBuff[j] != fs.ReadByte())
+                                break;
+                            j++;
+                        }
+                        if (j == 4)
+                        {
+                            MessageBox.Show("Don't need convert!");
+                            fs.Close();
+                            break;
+                        }
+                    }
+                    if (count == 2)
+                    {
+                        fs.Close();
+                        MessageBox.Show("Convert Completed");
+                        break;
+                    }
+                }
+            }
+            else
+                MessageBox.Show("No File");
+
+        }
     }
 }
